0a62103dfb24406ebb9c6bda9181c71298765f7a
KaleKemosabe <kale.kemosabe@gmail.com>
2021-03-05 18:37:24 +0200

    finish app structure

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -6,12 +6,24 @@ const initialState = {
     error: null
 }
 
-export default (state = initialState, action) => {
+const techReducers = (state = initialState, action) => {
     switch (action.type) {
         case GET_TECHS:
             return {
                 ...state,
                 techs: action.payload,
+                loading: false
+            };
+        case ADD_TECH:
+            return {
+                ...state,
+                techs: [...state.techs, action.payload],
+                loading: false
+            }
+        case DELETE_TECH:
+            return {
+                ...state,
+                techs: state.techs.filter(tech => tech.id !== action.payload),
                 loading: false
             }
         case SET_LOADING:
@@ -19,7 +31,16 @@ export default (state = initialState, action) => {
                 ...state,
                 loading: true
             }
+        case TECHS_ERROR:
+            console.log(action.payload);
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            }
         default:
             return state;
     }
 };
+
+export default techReducers;
