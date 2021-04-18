48d5d5bf209f530ee7ff112eb35a02f7c2a2c0a2
KaleKemosabe <kale.kemosabe@gmail.com>
2021-04-18 19:07:26 +0300

    add preloader

--- src/components/layout/Preloader.js
+++ src/components/layout/Preloader.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 
 const Preloader = () => {
+
+    const container = useRef(null)
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../preloader.json')
+        })
+    }, [])
+
     return (
-        <div className="progress purple lighten-4">
-            <div className="indeterminate purple"></div>
-        </div>
+        <div className="container" ref={container} style={{height: "200px", marginTop: "20vh"}}></div>
     )
 }
 
